6fa8453cdee42d777bb3102dc513a67fee1577ac
Ilya Sokolov <developer.sokolov@gmail.com>
2022-04-06 08:26:13 +0300

    stable generate x509 cert

--- server.c
+++ server.c
@@ -57,10 +57,14 @@ SSL_CTX *InitServerCTX(void) {
 }
 
 void LoadCertificates(SSL_CTX *ctx, char *CertFile, char *KeyFile) {
-    if (SSL_CTX_use_certificate_file(ctx, CertFile, SSL_FILETYPE_PEM) <= 0) {
+//    if (SSL_CTX_use_certificate_file(ctx, CertFile, SSL_FILETYPE_PEM) <= 0) {
+    if (SSL_CTX_use_certificate_chain_file(ctx, CertFile) <= 0) {
+//        if (SSL_CTX_use_certificate_file(ctx, CertFile, SSL_FILETYPE_ASN1) <= 0) {
         ERR_print_errors_fp(stderr);
+        SSL_CTX_free(ctx);
         abort();
     }
+//    }
     if (SSL_CTX_use_PrivateKey_file(ctx, KeyFile, SSL_FILETYPE_PEM) <= 0) {
         ERR_print_errors_fp(stderr);
         abort();
@@ -116,51 +120,126 @@ void Servlet(SSL *ssl) {
     close(sd);
 }
 
-void GenerateX509Cert() {
-    EVP_PKEY *pkey;
-    pkey = EVP_PKEY_new();
-    RSA *rsa;
-    rsa = RSA_generate_key(
-            4096,
-            RSA_F4,
-            NULL,
-            NULL
-    );
-    printf("value RSA: %d\n", rsa != NULL);
-    EVP_PKEY_assign_RSA(pkey, rsa);
-    X509 *x509;
-    x509 = X509_new();
+
+EVP_PKEY *generate_key() {
+    /* Allocate memory for the EVP_PKEY structure. */
+    EVP_PKEY *pkey = EVP_PKEY_new();
+    if (!pkey) {
+        printf("Unable to create EVP_PKEY structure.\n");
+        return NULL;
+    }
+
+    /* Generate the RSA key and assign it to pkey. */
+    RSA *rsa = RSA_generate_key(2048, RSA_F4, NULL, NULL);
+    if (!EVP_PKEY_assign_RSA(pkey, rsa)) {
+        printf("Unable to generate 2048-bit RSA key.\n");
+        EVP_PKEY_free(pkey);
+        return NULL;
+    }
+
+    /* The key has been generated, return it. */
+    return pkey;
+}
+
+/* Generates a self-signed x509 certificate. */
+X509 *generate_x509(EVP_PKEY *pkey) {
+    /* Allocate memory for the X509 structure. */
+    X509 *x509 = X509_new();
+    if (!x509) {
+        printf("Unable to create X509 structure.\n");
+        return NULL;
+    }
+
+    /* Set the serial number. */
     ASN1_INTEGER_set(X509_get_serialNumber(x509), 1);
+
+    /* This certificate is valid from now until exactly one year from now. */
     X509_gmtime_adj(X509_get_notBefore(x509), 0);
     X509_gmtime_adj(X509_get_notAfter(x509), 31536000L);
+
+    /* Set the public key for our certificate. */
     X509_set_pubkey(x509, pkey);
-    X509_NAME *name;
-    name = X509_get_subject_name(x509);
-    X509_NAME_add_entry_by_txt(name, "C", MBSTRING_ASC,
-                               (unsigned char *) "RU", -1, -1, 0);
-    X509_NAME_add_entry_by_txt(name, "O", MBSTRING_ASC,
-                               (unsigned char *) "RedByte", -1, -1, 0);
-    X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC,
-                               (unsigned char *) "localhost", -1, -1, 0);
+
+    /* We want to copy the subject name to the issuer name. */
+    X509_NAME *name = X509_get_subject_name(x509);
+
+    /* Set the country code and common name. */
+    X509_NAME_add_entry_by_txt(name, "C", MBSTRING_ASC, (unsigned char *) "RU", -1, -1, 0);
+    X509_NAME_add_entry_by_txt(name, "O", MBSTRING_ASC, (unsigned char *) "RedByte", -1, -1, 0);
+    X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC, (unsigned char *) "localhost", -1, -1, 0);
+
+    /* Now set the issuer name. */
     X509_set_issuer_name(x509, name);
-    X509_sign(x509, pkey, EVP_sha1());
-    FILE *key_file;
-    key_file = fopen("key.pem", "wb");
-    PEM_write_PrivateKey(
-            key_file,
-            pkey,
-            EVP_des_ede3_cbc(),
-            "passphrase",
-            10,
-            NULL,
-            NULL
-    );
-    FILE *cert_file;
-    cert_file = fopen("cert.pem", "wb");
-    PEM_write_X509(
-            cert_file,
-            x509
-    );
+
+    /* Actually sign the certificate with our key. */
+    if (!X509_sign(x509, pkey, EVP_sha1())) {
+        printf("Error signing certificate. \n");
+        X509_free(x509);
+        return NULL;
+    }
+
+    return x509;
+}
+
+int write_to_disk(EVP_PKEY *pkey, X509 *x509) {
+    /* Open the PEM file for writing the key to disk. */
+    FILE *pkey_file = fopen("key.pem", "wb");
+    if (!pkey_file) {
+        printf("Unable to open \"key.pem\" for writing.\n");
+        return 0;
+    }
+
+    /* Write the key to disk. */
+    int ret = PEM_write_PrivateKey(pkey_file, pkey, NULL, NULL, 0, NULL, NULL);
+    fclose(pkey_file);
+
+    if (!ret) {
+        printf("Unable to write private key to disk.\n");
+        return 0;
+    }
+
+    /* Open the PEM file for writing the certificate to disk. */
+    FILE *x509_file = fopen("cert.pem", "wb");
+    if (!x509_file) {
+        printf("Unable to open cert.pem for writing.\n");
+        return 0;
+    }
+
+    /* Write the certificate to disk. */
+    ret = PEM_write_X509(x509_file, x509);
+    fclose(x509_file);
+
+    if (!ret) {
+        printf("Unable to write certificate to disk.\n");
+        return 0;
+    }
+
+    return 1;
+}
+
+int GenerateX509Cert() {
+    EVP_PKEY *pkey = generate_key();
+    if (!pkey)
+        return 1;
+    printf("Generating x509 certificate...\n");
+
+    X509 *x509 = generate_x509(pkey);
+    if (!x509) {
+        EVP_PKEY_free(pkey);
+        return 1;
+    }
+
+    printf("Writing key and certificate to disk...\n");
+
+    int ret = write_to_disk(pkey, x509);
+    EVP_PKEY_free(pkey);
+    X509_free(x509);
+
+    if (ret) {
+        printf("Success!\n");
+        return 0;
+    } else
+        return 1;
 }
 
 int main(int count, char *Argc[]) {
@@ -176,12 +255,13 @@ int main(int count, char *Argc[]) {
         printf("Usage: %s <portnum>\n", Argc[0]);
         exit(0);
     }
-    GenerateX509Cert();
     SSL_library_init();
+    int ret = GenerateX509Cert();
+    printf("-------GenerateX509Cert: %d \n", ret);
     portnum = Argc[1];
     printf("port in now: %d \n", atoi(Argc[1]));
     ctx = InitServerCTX();
-    LoadCertificates(ctx, "cert.pem", "key.pem"); //TODO: move to arg
+    LoadCertificates(ctx, "cert.pem", "key.pem");
 
     server = OpenListener(atoi(portnum));
     while (1) {
